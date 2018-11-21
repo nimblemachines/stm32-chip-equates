92f66306ce9d7481c14adc3337ea6baf70e3699c
David Frech <david@nimblemachines.com>
2018-11-20 18:12:56 -0800

    Changed to STM32CubeFx. Much better. But still problems!
    The .h for the F103 (the ST-LINK) is very strange. No comments, the GPIO
    typedefs have no entries for the MODER etc. The comments with the
    offsets for OB are wrong, or seem to be. They don't match the offsets
    for the other chips...
    
    The F4 .h (for F4 disco board) lacks USB info, even though the chip
    totally supports it.
    
    CAN is still broken. HRTIM is broken.
    
    Parsing with patterns is a true PITA. But ATM, except for the embedded
    typedef problem (CAN and HRTIM), I'm getting pretty good results.

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 # Preprocess ST StdPeriph library files into Forth.
 
+# ST-LINK chip is an STM32F103C8T6; use stm32f103xb.h
+
 cpp=	cpp -fdirectives-only -P -nostdinc -I fake_include/ -DDELETE_ME
 
-HFILES=		stm32f0xx.h stm32f30x.h stm32f4xx.h stm32l1xx.h
+#                  F0 disco       ST-LINK    F303 disco      F4 disco    32L disco
+HFILES=		stm32f051x8.h stm32f103xb.h stm32f303xc.h stm32f407xx.h stm32l152xb.h
+
 MUFILES=	$(HFILES:.h=.mu4)
 
-all : stm32f0xx.mu4 stm32f30x.mu4 stm32f4xx.mu4 stm32l1xx.mu4
+all : $(MUFILES)
 
 clean :
 	rm -f $(HFILES) $(MUFILES)
@@ -13,6 +17,8 @@ clean :
 $(HFILES) $(MUFILES) : Makefile
 
 $(MUFILES) : c2forth.lua
+
+### StdPeriph sources deprecated
 
 stm32f0xx.h : STM32F0xx_StdPeriph_Lib_V1.5.0/Libraries/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h
 	$(cpp) -DSTM32F051 $< | sed -e '1,/DELETE_ME/d' > $@
@@ -25,6 +31,28 @@ stm32f4xx.h : STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/CMSIS/Device/ST/STM32
 
 stm32l1xx.h : STM32L1xx_StdPeriph_Lib_V1.3.1/Libraries/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h
 	$(cpp) -DSTM32L1XX_MD $< | sed -e '1,/DELETE_ME/d' > $@
+
+### Using STM32Cube instead
+
+# For re-programming the ST-LINK?
+stm32f103xb.h : STM32Cube_FW_F1_V1.6.0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
+	cp $< $@
+
+# STMF0 Discovery
+stm32f051x8.h : STM32Cube_FW_F0_V1.9.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f051x8.h
+	cp $< $@
+
+# STMF303 Discovery
+stm32f303xc.h : STM32Cube_FW_F3_V1.10.0/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h
+	cp $< $@
+
+# STMF4 Discovery
+stm32f407xx.h : STM32Cube_FW_F4_V1.21.0/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
+	cp $< $@
+
+# STM32L Discovery
+stm32l152xb.h : STM32Cube_FW_L1_V1.8.0/Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l152xb.h
+	cp $< $@
 
 %.mu4 : %.h
 	lua c2forth.lua $< > $@
--- c2forth.lua
+++ c2forth.lua
@@ -1,97 +1,30 @@
 -- Impossible task of reading chip .h file and trying to generate
 -- Forth-style equates.
 
--- Let's tackle the typedefs for each peripheral first.
-
-foo = [[
-#define FLASH_BASE            ((uint32_t)0x08000000) /*!< FLASH base address in the alias region */
-#define SRAM_BASE             ((uint32_t)0x20000000) /*!< SRAM base address in the alias region */
-#define PERIPH_BASE           ((uint32_t)0x40000000) /*!< Peripheral base address in the alias region */
-
-#define SRAM_BB_BASE          ((uint32_t)0x22000000) /*!< SRAM base address in the bit-band region */
-#define PERIPH_BB_BASE        ((uint32_t)0x42000000) /*!< Peripheral base address in the bit-band region */
-
-#define FSMC_R_BASE           ((uint32_t)0xA0000000) /*!< FSMC registers base address */
-
-/*!< Peripheral memory map */
-#define APB1PERIPH_BASE       PERIPH_BASE
-#define APB2PERIPH_BASE       (PERIPH_BASE + 0x10000)
-#define AHBPERIPH_BASE        (PERIPH_BASE + 0x20000)
-
-#define TIM2_BASE             (APB1PERIPH_BASE + 0x0000)
-#define TIM3_BASE             (APB1PERIPH_BASE + 0x0400)
-#define TIM4_BASE             (APB1PERIPH_BASE + 0x0800)
-#define TIM5_BASE             (APB1PERIPH_BASE + 0x0C00)
-#define TIM6_BASE             (APB1PERIPH_BASE + 0x1000)
-#define TIM7_BASE             (APB1PERIPH_BASE + 0x1400)
-#define LCD_BASE              (APB1PERIPH_BASE + 0x2400)
-#define RTC_BASE              (APB1PERIPH_BASE + 0x2800)
-#define WWDG_BASE             (APB1PERIPH_BASE + 0x2C00)
-#define IWDG_BASE             (APB1PERIPH_BASE + 0x3000)
-#define SPI2_BASE             (APB1PERIPH_BASE + 0x3800)
-#define SPI3_BASE             (APB1PERIPH_BASE + 0x3C00)
-#define USART2_BASE           (APB1PERIPH_BASE + 0x4400)
-#define USART3_BASE           (APB1PERIPH_BASE + 0x4800)
-#define UART4_BASE            (APB1PERIPH_BASE + 0x4C00)
-#define UART5_BASE            (APB1PERIPH_BASE + 0x5000)
-#define I2C1_BASE             (APB1PERIPH_BASE + 0x5400)
-#define I2C2_BASE             (APB1PERIPH_BASE + 0x5800)
-#define PWR_BASE              (APB1PERIPH_BASE + 0x7000)
-#define DAC_BASE              (APB1PERIPH_BASE + 0x7400)
-#define COMP_BASE             (APB1PERIPH_BASE + 0x7C00)
-#define RI_BASE               (APB1PERIPH_BASE + 0x7C04)
-#define OPAMP_BASE            (APB1PERIPH_BASE + 0x7C5C)
-
-#define SYSCFG_BASE           (APB2PERIPH_BASE + 0x0000)
-#define EXTI_BASE             (APB2PERIPH_BASE + 0x0400)
-#define TIM9_BASE             (APB2PERIPH_BASE + 0x0800)
-#define TIM10_BASE            (APB2PERIPH_BASE + 0x0C00)
-#define TIM11_BASE            (APB2PERIPH_BASE + 0x1000)
-#define ADC1_BASE             (APB2PERIPH_BASE + 0x2400)
-#define ADC_BASE              (APB2PERIPH_BASE + 0x2700)
-#define SDIO_BASE             (APB2PERIPH_BASE + 0x2C00)
-#define SPI1_BASE             (APB2PERIPH_BASE + 0x3000)
-#define USART1_BASE           (APB2PERIPH_BASE + 0x3800)
-
-#define GPIOA_BASE            (AHBPERIPH_BASE + 0x0000)
-#define GPIOB_BASE            (AHBPERIPH_BASE + 0x0400)
-#define GPIOC_BASE            (AHBPERIPH_BASE + 0x0800)
-#define GPIOD_BASE            (AHBPERIPH_BASE + 0x0C00)
-#define GPIOE_BASE            (AHBPERIPH_BASE + 0x1000)
-#define GPIOH_BASE            (AHBPERIPH_BASE + 0x1400)
-#define GPIOF_BASE            (AHBPERIPH_BASE + 0x1800)
-#define GPIOG_BASE            (AHBPERIPH_BASE + 0x1C00)
-#define CRC_BASE              (AHBPERIPH_BASE + 0x3000)
-#define RCC_BASE              (AHBPERIPH_BASE + 0x3800)
-
-
-#define FLASH_R_BASE          (AHBPERIPH_BASE + 0x3C00) /*!< FLASH registers base address */
-#define OB_BASE               ((uint32_t)0x1FF80000)    /*!< FLASH Option Bytes base address */
-
-#define DMA1_BASE             (AHBPERIPH_BASE + 0x6000)
-#define DMA1_Channel1_BASE    (DMA1_BASE + 0x0008)
-#define DMA1_Channel2_BASE    (DMA1_BASE + 0x001C)
-#define DMA1_Channel3_BASE    (DMA1_BASE + 0x0030)
-#define DMA1_Channel4_BASE    (DMA1_BASE + 0x0044)
-]]
-
 fmt = string.format
 
 function hex(s)
     return tonumber(s, 16)
 end
 
--- Be careful with matching! Line ending seem to be CRLF, not just bare LF.
--- So \n won't match a line-ending!
+function prettify_comment(c)
+    c = c:gsub("^%s+$", "")
+         :gsub("%s*/%*!?<?%s*(..-)%s*%*/", "| %1")
+
+    return c
+end
+
 function typedefs(f)
     local periphs = {}
     for guts, name in f:gmatch("typedef struct(..-)([%w_]+)_TypeDef;") do
-        -- print(fmt("%s: %s", name, guts))
+        local fixedguts = guts:gsub("/%*!<([^*]-) *\n *(.-) *(Address.-)%*/", "/*!<%1 %2 %3*/")
+        --print(fmt("%s: %s: %s", name, guts, fixedguts))
         local regs = {}
         periphs[name] = regs
         local offset = 0
-        for bits, name, comment in guts:gmatch "uint(%d+)_t%s*(%S+);%s*/%*!<(..-)%*/" do
-            --print(fmt("%x %s | %s", offset, name, comment))
+        for bits, name, comment in fixedguts:gmatch "uint(%d+)_t%s*(%S+);(.-)\n" do
+            comment = prettify_comment(comment)
+            --print(fmt("%x %s %s", offset, name, comment))
             local reg = { name = name, offset = offset, comment = comment }
             array = name:match "%[(%d+)%]"
             if array then
@@ -123,8 +56,8 @@ function eval(base, e)
     if b then
         return base[b]
     end
-    -- Match bare hex value
-    local value = e:match "%(0x(%x+)%)"
+    -- Match bare hex value, with or without enclosing parens
+    local value = e:match "%(?0x(%x+)U?%)?"
     if value then
         return hex(value)
     end
@@ -147,11 +80,11 @@ end
 
 function instantiate(f, base, periphs)
     for pname, ptype, pbase in f:gmatch
-        "#define%s+([%w_]+)%s+%(%(([%w_]+)_TypeDef %*%)%s+([%w_]+)%)" do
+        "#define%s+([%w_]+)%s+%(%(([%w_]+)_TypeDef %*%)%s*([%w_]+)%)" do
         print()
+        --print(fmt("instantiate: %s %s %s", pname, ptype, pbase))
         for _, r in ipairs(periphs[ptype]) do
-            --print(fmt("%08x equ %-20s |%s", r.offset + base[pbase], pname .. "." .. r.name, r.comment))
-            print(fmt("%s equ %-20s |%s", muhex(r.offset + base[pbase]), pname .. "." .. r.name, r.comment))
+            print(fmt("%s equ %-20s %s", muhex(r.offset + base[pbase]), pname .. "." .. r.name, r.comment))
         end
     end
 end
@@ -161,7 +94,8 @@ function read_file(fname)
     if f then
         local contents = f:read "a"
         f:close()
-        return contents
+        -- If we are given CRLF line endings, change to LF only
+        return contents:gsub("\r\n", "\n")
     end
 end
 
