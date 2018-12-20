b9df89160c49c92560854484ac8bed7d4dd00e26
David Frech <david@nimblemachines.com>
2018-12-19 18:26:35 -0800

    Now generates register bitfields too! (and finds errors in .h files)
    I am inadvertently "linting" ST's .h files by doing this.
    
    Instead of simply copying .h files out of an STM32Cube zip file
    converting it, I'm either going to have to check in the .h files I care
    about (with changes to fix the brokenness and typos) or keep a set of
    patch files.
    
    Right now the registers are all printing, then the bitfields are all
    printed. They are *not* interleaved. (I may tinker with ways to make
    this happen.)
    
    Also, the second part of each field definition is a *mask* rather than a
    *width*. This is so in part because *one* of the masks defined in
    stm32l152xb.h (RI_ASCR1_CH_Msk) is defined as 0x3FCFFFF - which has gaps
    in it and can't be represented simply by a width! It should really be an
    *equate* and not a mask.
    
    I've changed the way registers are printed as well. They used to be
    <peripheral>.<register> but since all the fields are defined as
    <periph>_<register> I switched to that convention.
    
    I could have changed the fields instead. I'm not sure which I prefer!
    
    periph.reg is more like C.

--- c2forth.lua
+++ c2forth.lua
@@ -3,6 +3,19 @@
 
 fmt = string.format
 
+function logger(prefix)
+    return function(s)
+        io.stderr:write(fmt("%s: %s\n", prefix, s))
+    end
+end
+
+warn  = logger "Warning"
+debug = logger "Debug"
+
+function out(s)
+    io.stdout:write(s .. "\n")
+end
+
 -- fixes() changes file contents to make things easier later. See NOTES for
 -- explanation(s).
 function fixes(f)
@@ -18,6 +31,7 @@ end
 
 function prettify_comment(c)
     c = c:gsub("^%s+$", "")
+         :gsub("%s+", " ")
          :gsub("%s*/%*!?<?%s*(..-)%s*%*/", "| %1")
 
     return c
@@ -32,11 +46,11 @@ function exceptions(f)
     if guts then
         for name, vector, comment in guts:gmatch "([%w_]+)_IRQn%s*=%s*(%-?%d+),(.-)\n" do
             comment = prettify_comment(comment)
-            --print(fmt("%s %d %s", name, tonumber(vector), comment))
+            --debug(fmt("%s %d %s", name, tonumber(vector), comment))
             exc[#exc+1] = { name = name, vector = vector, comment = comment }
         end
     else
-        print("Hmm. No IRQn_Type was found.")
+        warn("Hmm. No IRQn_Type was found.")
     end
     return exc
 end
@@ -45,13 +59,13 @@ function typedefs(f)
     local periphs = {}
     for guts, name in f:gmatch "typedef struct(..-)([%w_]+)_TypeDef;" do
         local fixedguts = guts:gsub("/%*!<([^*]-) *\n *(.-) *(Address.-)%*/", "/*!<%1 %2 %3*/")
-        --print(fmt("%s: %s: %s", name, guts, fixedguts))
+        --debug(fmt("%s: %s: %s", name, guts, fixedguts))
         local regs = {}
         periphs[name] = regs
         local offset = 0
         for ctype, name, comment in fixedguts:gmatch "([%w_]+)%s+(%S+);(.-)\n" do
             comment = prettify_comment(comment)
-            --print(fmt("%x %s %s", offset, name, comment))
+            --debug(fmt("%x %s %s", offset, name, comment))
             local reg = { name = name, offset = offset, comment = comment }
             local size = 4  -- sanity
             local bits = ctype:match "uint(%d+)_t"
@@ -63,7 +77,7 @@ function typedefs(f)
                     size = periphs[struct].size
                     reg.struct = periphs[struct]
                 else
-                    print("Hmm. Couldn't figure out size of %s", name)
+                    warn("Hmm. Couldn't figure out size of %s", name)
                 end
             end
 
@@ -89,6 +103,85 @@ function typedefs(f)
     return periphs
 end
 
+-- Match _Pos, _Msk, and comment separately?
+function bitfields(f)
+    local fields = {}           -- array of fields
+    local fields_by_name = {}   -- table indexed by field name
+    local eval = function(name, expr)
+        local pos = expr:match "^%d+$"      -- bare number
+        if pos then
+            return true, pos
+        end
+        local prev
+        prev, pos = expr:match "([%w_]+)_Pos%s+%+%s+(%d+)"
+        if prev then
+            if fields_by_name[prev] and fields_by_name[prev].pos then
+                return true, fields_by_name[prev].pos + pos
+            end
+            warn(fmt("Ignoring %s which depends on undefined bitfield %s!",
+                name, prev))
+                return false, 0
+        end
+        warn(fmt("Hmm. Can't eval %s", expr))
+        return false, 0
+    end
+
+    -- Find bare shifted masks/values. Do this first! We define them as
+    -- "equates" rather than fields. Sometimes the value is 0000_0000 !!
+    for name, mask, comment in f:gmatch "([%w_]+)%s+%((0x%x+)U%)%s+(/%*!<.-%*/)" do
+        comment = prettify_comment(comment)
+        --debug(fmt("bare: %s %08x %s", name, mask, comment))
+        local field = { name = name, equ = mask, comment = comment }
+        fields_by_name[name] = field
+        fields[#fields+1] = field
+    end
+    -- Find positions
+    for name, expr in f:gmatch "([%w_]+)_Pos%s+%((.-%d+)U%)" do
+        local ok, pos = eval(name, expr)
+        if ok then
+            --debug(fmt("%s %s %d", name, expr, pos))
+            if fields_by_name[name] then
+                warn(fmt("Skipping redefinition of bitfield %s with pos %d", name, pos))
+            else
+                local field = { name = name, pos = pos }
+                fields_by_name[name] = field
+                fields[#fields+1] = field
+            end
+        end
+    end
+    -- Find masks
+    for name, mask in f:gmatch "([%w_]+)_Msk%s+%((0x%x+)U" do
+        --debug(fmt("%s %s", name, mask))
+        if fields_by_name[name] then
+            fields_by_name[name].mask = mask
+        else
+            warn(fmt("Skipping mask for missing bitfield %s", name))
+        end
+    end
+    -- Find comments
+    for name, name2, comment in f:gmatch "([%w_]+)%s+([%w_]+)_Msk%s+(/%*!<.-%*/)" do
+        comment = prettify_comment(comment)
+        --debug(fmt("%s %s %s", name, name2, comment))
+        -- Make sure names match
+        if name == name2 then
+            if fields_by_name[name] then
+                fields_by_name[name].comment = comment
+            else
+                warn(fmt("Skipping comment for missing bitfield %s", name))
+            end
+        end
+    end
+    table.sort(fields, function(x, y)
+        local function index(f)
+            return (f.name:match "%w+_%w+") ..
+                -- Sort fields before equates
+                (f.pos and fmt("A%02d", f.pos) or fmt("B%08x", f.equ))
+        end
+        return index(x) < index(y)
+    end)
+    return fields, fields_by_name
+end
+
 function eval(base, e)
     e = e:gsub("%(%s*uint32_t%s*%)", "")
     -- Match basename + offset
@@ -106,15 +199,15 @@ function eval(base, e)
     if value then
         return hex(value)
     end
-    print(fmt("Hmm. Couldn't eval %s", e))
+    warn(fmt("Hmm. Couldn't eval %s", e))
 end
 
 function base_addrs(f)
     local base = {}
     for p, expr in f:gmatch "#define%s+([%w_]+_BASE)%s+(..-)\n" do
-        --print(p, expr)
+        --debug(p, expr)
         base[p] = eval(base, expr)
-        --print(fmt("%s %x", p, base[p]))
+        --debug(fmt("%s %x", p, base[p]))
     end
     return base
 end
@@ -124,21 +217,21 @@ function muhex(num)
 end
 
 function vectors(exc)
-    print("\n( Vectors)\ndecimal")
+    out("\n( Vectors)\ndecimal")
     for _, e in ipairs(exc) do
-        print(fmt("%3d vector %-20s %s", e.vector, e.name, e.comment))
+        out(fmt("%3d vector %-20s %s", e.vector, e.name, e.comment))
     end
-    print("hex")
+    out("hex")
 end
 
 function instantiate(f, base, periphs)
     local print_reg, print_regs
     print_reg = function(r, pname, subscript, pbase)
         if r.struct then
-            print_regs(r.struct, pname .. "." .. r.name .. subscript, pbase + r.offset)
+            print_regs(r.struct, pname .. "_" .. r.name .. subscript, pbase + r.offset)
         else
-            print(fmt("%s equ %-26s %s", muhex(r.offset + pbase),
-                pname .. "." .. r.name .. subscript, r.comment))
+            out(fmt("%s equ %-26s %s", muhex(r.offset + pbase),
+                pname .. "_" .. r.name .. subscript, r.comment))
         end
     end
     print_regs = function(regs, pname, pbase)
@@ -155,9 +248,43 @@ function instantiate(f, base, periphs)
 
     for pname, ptype, pbase in f:gmatch
         "#define%s+([%w_]+)%s+%(%(([%w_]+)_TypeDef %*%)%s*([%w_]+)%)" do
-        print()
-        --print(fmt("instantiate: %s %s %s", pname, ptype, pbase))
+        out ""
+        --debug(fmt("instantiate: %s %s %s", pname, ptype, pbase))
         print_regs(periphs[ptype], pname, base[pbase])
+    end
+end
+
+function print_bitfields(fields)
+    local lastreg = ""
+    for _, f in ipairs(fields) do
+        local reg = f.name:match "%w+_%w+"
+        if reg ~= lastreg then
+            out ""
+        end
+        if f.equ then
+            -- Equate, not bit field.
+            out(fmt("      %s equ    %-26s %s", muhex(f.equ), f.name, f.comment))
+        else
+            if convert_mask_to_width then
+                -- Convert mask into width
+                --debug(fmt("mask = %08x", f.mask))
+                local width = 0
+                local m = f.mask + 1
+                while m ~= 1 do
+                    m = m >> 1
+                    width = width + 1
+                end
+
+                --debug(fmt("mask %08x => width %d", f.mask, width))
+                out(fmt("  #%02d #%02d field  %-26s %s", f.pos, width, f.name,
+                    f.comment or ""))
+            else
+                --debug(fmt("%s", f.name))
+                out(fmt("  #%02d %s field  %-26s %s", f.pos, muhex(f.mask), f.name,
+                    f.comment or ""))
+            end
+        end
+        lastreg = reg
     end
 end
 
@@ -175,10 +302,12 @@ function doit()
     local f = fixes(read_file(arg[1]))
     local exc = exceptions(f)
     local periphs = typedefs(f)
+    local fields, fields_by_name = bitfields(f)
     local base = base_addrs(f)
-    print(fmt("( Chip equates for %s)", arg[1]:match "^[%w]+"))
+    out(fmt("( Chip equates for %s)", arg[1]:match "^[%w]+"))
     vectors(exc)
     instantiate(f, base, periphs)
+    print_bitfields(fields)
 end
 
 doit()
